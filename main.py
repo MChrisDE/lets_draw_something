e1eb94191e94a792b966d0626aa0afa651350a91
Stefan Lukas <afc11hn@hotmail.de>
2017-07-15 19:37:40 +0200

    Simplify Canvas class

--- main.py
+++ main.py
@@ -4,56 +4,57 @@ canvas_width = 500
 canvas_height = 500
 
 
-class Screen:
-    def __init__(self, tkmaster, width, height):
-        self.canvas = Canvas(tkmaster, width=width, height=height)
-        self.canvas.grid(row=0, column=0, padx=10, pady=10)
-        self.canvas.bind("<B1-Motion>", paint)
-        self.canvas.config(highlightbackground="black")
+class DrawCanvas(Canvas):
+	def __init__(self, tkmaster, **kw):
+		super().__init__(tkmaster, **kw)
+		self.grid(row=0, column=0, padx=10, pady=10)
+		self.bind("<B1-Motion>", paint)
+		self.config(highlightbackground="black")
 
 
 class ColorFrame:
-    def __init__(self, tkmaster):
-        self.frame = Frame(tkmaster)
-        self.frame.grid(row=0, column=1)
-        self.colors = ["black", "white", "red", "green", "blue", "yellow"]
-        buttons = []
-        for i in range(0, len(self.colors)):
-            buttons.append(Color(self.frame, self.colors[i], i))
+	def __init__(self, tkmaster):
+		self.frame = Frame(tkmaster)
+		self.frame.grid(row=0, column=1)
+		self.colors = ["black", "white", "red", "green", "blue", "yellow"]
+		buttons = []
+		for i in range(0, len(self.colors)):
+			buttons.append(Color(self.frame, self.colors[i], i))
 
 
 class Color:
-    def __init__(self, tkmaster, bcolor, row):
-        self.color = bcolor
-        self.button = Button(tkmaster, text="      ", command=self.changecolor)
-        self.button.config(background=bcolor)
-        self.button.grid(row=row, column=0)
+	def __init__(self, tkmaster, bcolor, row):
+		self.color = bcolor
+		self.button = Button(tkmaster, text="      ", command=self.changecolor)
+		self.button.config(background=bcolor)
+		self.button.grid(row=row, column=0)
 
-    def changecolor(self):
-        global color
-        color = self.color
+	def changecolor(self):
+		global color
+		color = self.color
 
 
 def paint(event):
-    global canvas, canvas_height, canvas_width
-    x, y = event.x, event.y
-    dx = canvas_width - x
-    dy = canvas_height - y
-    canvas.canvas.create_rectangle(x, y, (x + 1), (y + 1), outline=color)
-    canvas.canvas.create_rectangle(dx, y, (dx + 1), (y + 1), outline=color)
-    canvas.canvas.create_rectangle(x, dy, (x + 1), (dy + 1), outline=color)
-    canvas.canvas.create_rectangle(dx, dy, (dx + 1), (dy + 1), outline=color)
+	global canvas, canvas_height, canvas_width
+	x, y = event.x, event.y
+	dx = canvas_width - x
+	dy = canvas_height - y
+	canvas.create_rectangle(x, y, (x + 1), (y + 1), outline=color)
+	canvas.create_rectangle(dx, y, (dx + 1), (y + 1), outline=color)
+	canvas.create_rectangle(x, dy, (x + 1), (dy + 1), outline=color)
+	canvas.create_rectangle(dx, dy, (dx + 1), (dy + 1), outline=color)
 
 
 def deleteall():
-    canvas.canvas.delete("all")
+	canvas.delete("all")
+
 
 color = "black"
 
 master = Tk()
 master.title("lets draw something")
 
-canvas = Screen(master, canvas_width, canvas_height)
+canvas = DrawCanvas(master, width=canvas_width, height=canvas_height)
 
 colorframe = ColorFrame(master)
 
